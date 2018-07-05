a5f2362f6b88e439cbd789af6deeeb8df005ba22
Mryixiansheng <675926115@qq.com>
2018-07-05 15:08:31 +0800

    自定义键盘组件

--- src/pages/keyboardShow.vue
+++ src/pages/keyboardShow.vue
@@ -1,13 +1,30 @@
 <template>
     <div>
-      keyboard
+      <input type="text" v-model="value">
+      <key-board  @submit-event='handleSubmit' @change-event='handleChange'></key-board>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+/* eslint-disable */
+import keyBoard from '../components/keyboard.vue'
 export default {
   data () {
-    return {}
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    handleChange(value, currentValue) {
+      console.log(value, currentValue);
+      this.value = value;
+    },
+    handleSubmit() {
+      console.log('submit: ' + this.value);
+    }
+  },
+  components: {
+    keyBoard
   }
 }
 </script>
